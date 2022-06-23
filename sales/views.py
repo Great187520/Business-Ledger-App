9c375a0eb3d34ab20f68b5364e7cf20f44a8e6bd
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-23 22:31:09 +0100

    Add files via upload

--- sales/views.py
+++ sales/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Sale
+from .forms import SalesSearchForm
+import pandas as pd
 
 # Create your views here.
 def home_view(request):
-    hello = 'hello the truth seeker'
-    return render(request, 'sales/home.html', {'hello': hello})
+    sales_df = None
+    positions_df = None
+    form = SalesSearchForm(request.POST or None)
+
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+        print(date_from, date_to, chart_type)
+
+        sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        if len(qs) > 0:
+            sales_df= pd.DataFrame(qs.values())
+            positions_df = []
+
+            
+
+            sales_df = sales_df.to_html()
+            print(sales_df)
+        else:
+            print('no data')
+        
+    context = {
+        'form': form,
+        'sales_df': sales_df
+    }
+    return render(request, 'sales/home.html', context)
+
+class SaleListView(ListView):
+    model = Sale
+    template_name = 'sales/main.html'
+
+class SaleDetailView(DetailView):
+    model = Sale
+    template_name = 'sales/detail.html'
