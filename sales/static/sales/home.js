67a1594dc663b3e8198416fe07716091fb8e3b58
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-25 03:09:34 +0100

    Add files via upload

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+console.log('Hello World')
+
+const reportBtn = document.getElementById('report-btn')
+const img = document.getElementById('img')
+const modalBody = document.getElementById('modal-body')
+
+console.log(reportBtn)
+console.log(img)
+
+if (img){
+    reportBtn.classList.remove('not-visible')
+}
+
+reportBtn.addEventListener('click', ()=> {
+    console.log('Clicked')
+})
--- sales/views.py
+++ sales/views.py
@@ -2,20 +2,26 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sale
 from .forms import SalesSearchForm
+from reports.forms import ReportForm
 import pandas as pd
-from .utils import get_customer_from_id, get_salesman_from_id
+from .utils import get_customer_from_id, get_salesman_from_id, get_chart
 
 # Create your views here.
 def home_view(request):
     sales_df = None
     positions_df = None
     merged_df = None
-    form = SalesSearchForm(request.POST or None)
+    df = None
+    chart = None
+    no_data = None
+    search_form = SalesSearchForm(request.POST or None)
+    report_form = ReportForm()
 
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
+        results_by = request.POST.get('results_by')
         #print(date_from, date_to, chart_type)
 
         sale_qs = Sale.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
@@ -40,18 +46,27 @@ def home_view(request):
 
             positions_df = pd.DataFrame(positions_data)
             merged_df = pd.merge(sales_df, positions_df, on='sales_id')
-           
+
+            df = merged_df.groupby('transaction_id', as_index=False)['price'].agg('sum')
+            chart = get_chart(chart_type, sales_df, results_by)
+            #print('chart', chart)
+
             sales_df = sales_df.to_html()
             positions_df = positions_df.to_html()
             merged_df  = merged_df.to_html()
+            df = df.to_html()
         else:
-            print('no data')
+            no_data = 'No data is available in this date range'
         
     context = {
-        'form': form,
+        'search_form': search_form,
+        'report_form': report_form,
         'sales_df': sales_df,
         'positions_df': positions_df,
         'merged_df': merged_df,
+        'df': df,
+        'chart': chart,
+        'no_data': no_data,
     }
     return render(request, 'sales/home.html', context)
 
