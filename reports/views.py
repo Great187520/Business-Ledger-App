693922ac0b1c4d53a3d96ccf0797439ef29cf226
Eleke Great Chima <59459106+Great187520@users.noreply.github.com>
2022-06-27 22:42:02 +0100

    Add files via upload

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from profiles.models import Profile
+from django.http import JsonResponse
 
 # Create your views here.
+def create_report_view(request):
+    if request.is_ajax():
+        name = request.POST.get('name')
+        remarks = request.POST.get('remarks')
+        image = request.POST.get('image')
+
+        author =Profile.objects.get(user=request.user) 
+        return JsonResponse({'msg': 'send'})
+    return JsonResponse({})
